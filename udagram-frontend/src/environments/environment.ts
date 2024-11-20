5c363b19922f25a771f12c898d87c61b59feb9bb
t.muehlbauer <thomas-muehlbauer@live.com>
2024-11-20 17:56:17 +0100

    connected frontend to reverseproxy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad38469ba34a449d0a2710ef627bd743-135105199.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
